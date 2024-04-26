f36c96fc3d100c147f30acedac7eeead41dae3a1
kouroshMojdehi <k.rashidimojdehi@gmail.com>
2024-04-26 06:45:14 +0330

    sortByName completed

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,40 +9,71 @@ import java.util.*;
 public class Parser {
     static List<Country> countries = new ArrayList<>();
 
-    public List<Country> sortByName(){
+    public static List<Country> sortByName(){
         List<Country> sortedByName = new ArrayList<>(countries);
-        // Sort countries alphabetically (least)
-        //TODO
+        int size = sortedByName.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+
+                String name1 = sortedByName.get(i).getName();
+                String name2 = sortedByName.get(j).getName();
+                if (name1.compareTo(name2) > 0) {
+                    Country temp = sortedByName.get(i);
+                    sortedByName.set(i , sortedByName.get(j));
+                    sortedByName.set(j , sortedByName.get(i));
+                }
+            }
+        }
         return  sortedByName;
     }
 
-    public List<Country> sortByPopulation(){
+    public static List<Country> sortByPopulation(){
         List<Country> sortedByPopulation = new ArrayList<>(countries);
         // Sort countries by population (most)
         //TODO
         return sortedByPopulation;
     }
 
-    public List<Country> sortByArea(){
+    public static List<Country> sortByArea(){
         List<Country> sortedByArea = new ArrayList<>(countries);
         // Sort countries by area (most)
         //TODO
         return sortedByArea;
     }
 
-    public void setUp() throws IOException {
+    public static void  setUp() throws IOException {
 
         //Parse the HTML file using Jsoup
-        //TODO
+        File file = new File("src/Resources/country-list.html");
+        Document doc = Jsoup.parse(file, "UTF-8");
+        Elements elements = doc.getElementsByClass("col-md-4 country");
+        Country newCountry ;
+        for( Element i : elements){
+            String name = i.getElementsByClass("country-name").first().text();
+            String capital = i.getElementsByClass("country-capital").first() . text();
+            String populationtext = (i .getElementsByClass("country-population").first().text() );
+            int population = Integer.parseInt( populationtext );
+            String areatext = i. getElementsByClass("country-area").first().text();
+            double area = Double.parseDouble( areatext);
+            newCountry=  new Country( name , capital , population , area);
+            countries.add(newCountry);
+        }
 
-        // Extract data from the HTML
-        //TODO
+
+
+        // Iterate through each country element
+
 
         // Iterate through each country div to extract country data
         //TODO
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //you can test your code here before you run the unit tests ;)
+        setUp();
+        for ( Country i : sortByName()){
+
+        }
+
     }
 }
